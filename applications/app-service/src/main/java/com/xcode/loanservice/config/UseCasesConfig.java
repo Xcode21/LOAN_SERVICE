9f576d15433841bee965954da95127db7d1c0c84
xCode <contactosystemr@gmail.com>
2025-09-07 11:39:52 -0500

    version 01

---
+++ applications/app-service/src/main/java/com/xcode/loanservice/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.xcode.loanservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.xcode.loanservice.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
